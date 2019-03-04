8bb0b27a6e7f3cd8c34bb157d15aa457234a5d12
17373323 <17373323_2019_jac@stu-115.(none)>
2019-03-04 14:45:45 +0800

    finish the fibo.c

--- src/fibo.c
+++ src/fibo.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
-int fibo(){
-    
+int fibo(int num){
+    if(num==1) return 1;
+    else if (num==2)return 1;
+    else return fibo(num-1)+fibo(num-2);
 }
 
 int main(){
     int var;
     scanf("%d", &var);
-
+    int i ;
+    for(i=1;i<=var;i++) printf("%d ",fibo(i));
     return 0;
 }
