de4bfeaee9eac95cdf5aec02f1b298532c8c2650
10.210.0.67 <10.210.0.67@example.com>
2019-05-10 17:05:03 +0800

    Initial lab5

--- init/init.c
+++ init/init.c
@@ -7,38 +7,31 @@
 
 extern char aoutcode[];
 extern char boutcode[];
-extern int debug_mode;
 
 void mips_init()
 {
 	printf("init.c:\tmips_init() is called\n");
-	mips_detect_memory();
-//`	set_debug();
 
+	// Lab 2 memory management initialization functions
+	mips_detect_memory();
 	mips_vm_init();
 	page_init();
-	
+	//page_check();
+
+	// Lab 3 user environment initialization functions
 	env_init();
-	
-	if(debug_mode) printf("[DEBUG] before ENV_CREAT\n");
-
-	ENV_CREATE(user_test_code);
-
-//	ENV_CREATE(user_test_code);
-//	ENV_CREATE(user_test_code);
-//	ENV_CREATE(user_test_code);
-//	ENV_CREATE(user_test_code);
-//	ENV_CREATE(user_fktest);
-//	ENV_CREATE(user_pingpong);
-	
-	if(debug_mode) printf("[DEBUG] ENV_CREATE OK\n");
-
-
-    trap_init();
+	// initialize exception vector.
+	trap_init();
+	// initialize PIT.
 	kclock_init();
 
-	if(debug_mode) printf("[DEBUG] before while 1\n");	
-	while(1);
+	// Create process using macro 'ENC_CREATE'
+	// For more details about 'ENV_CREATE', see include/env.h
+
+	panic("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+
+	while (1);
+
 	panic("init.c:\tend of mips_init() reached!");
 }
 
@@ -47,19 +40,19 @@ void bcopy(const void *src, void *dst, size_t len)
 	void *max;
 
 	max = dst + len;
+
 	// copy machine words while possible
-	while (dst + 3 < max)
-	{
+	while (dst + 3 < max) {
 		*(int *)dst = *(int *)src;
-		dst+=4;
-		src+=4;
+		dst += 4;
+		src += 4;
 	}
+
 	// finish remaining 0-3 bytes
-	while (dst < max)
-	{
+	while (dst < max) {
 		*(char *)dst = *(char *)src;
-		dst+=1;
-		src+=1;
+		dst += 1;
+		src += 1;
 	}
 }
 
@@ -70,21 +63,17 @@ void bzero(void *b, size_t len)
 	max = b + len;
 
 	//printf("init.c:\tzero from %x to %x\n",(int)b,(int)max);
-	
+
 	// zero machine words while possible
 
-	while (b + 3 < max)
-	{
+	while (b + 3 < max) {
 		*(int *)b = 0;
-		b+=4;
+		b += 4;
 	}
-	
+
 	// finish remaining 0-3 bytes
-	while (b < max)
-	{
+	while (b < max) {
 		*(char *)b++ = 0;
 	}
 
-	if(debug_mode) printf("[DEBUG] we are in bzero\n");
-	
 }
