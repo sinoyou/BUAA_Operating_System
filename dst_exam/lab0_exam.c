74a93c02082cd49f8a5a523c808f25c67009b1b2
sinoyou <youzn99@qq.com>
2020-11-28 20:10:13 +0800

    delete notes and add readme

--- dst_exam/lab0_exam.c
+++
@@ -1,2 +0,0 @@
-dir/dir0/lab0_x
-dir/dir0/lab0_0:4:bula hello OS lab0 lalalal
--- lab5hm.sh
+++
@@ -1,8 +0,0 @@
-#########################################################################
-# File Name: vm.sh
-# Author: Lebway
-# mail: lebway@foxmail.com
-# Created Time: Sun Apr 21 09:46:53 2019
-#########################################################################
-#!/bin/bash
-/OSLAB/gxemul -E testmips -C R3000 -M 64 -d gxemul/fs.img  gxemul/vmlinux
